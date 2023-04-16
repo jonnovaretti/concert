d9fac8717ab48716195ce3ea46ac06631ee1ddfb
Jonathan Novaretti <jonn.novaretti@gmail.com>
2023-04-16 20:04:04 -0300

    Starting with Stimulus controllers and actions

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -11,7 +11,7 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
+application.debug = true
 window.Stimulus   = application
 
 export { application }
