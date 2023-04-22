fc741e7e92437c3ba8045fae02b8fa54b0dcb38a
Jonathan Novaretti <jonn.novaretti@gmail.com>
2023-04-22 07:03:19 -0300

    Stimulus Using values and target

--- app/javascript/controllers/favorite_toggle_controller.ts
+++ app/javascript/controllers/favorite_toggle_controller.ts
@@ -1,7 +1,37 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class FavoriteToggleController extends Controller {
-  toggle({params: { text }}): void {
-    console.log(text);
+  static targets = ["elementToHide", "elementWithText"]
+  elementToHideTarget: HTMLElement
+  elementWithTextTarget: HTMLElement
+
+  static values = { visible: Boolean }
+  visibleValue: boolean
+
+  connect(): void {
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  toggle(): void{
+    this.flipState()
+    this.updateHiddenClass()
+    this.updateText()
+  }
+
+  flipState(): void {
+    this.visibleValue = !this.visibleValue
+  }
+
+  updateHiddenClass(): void {
+    this.elementToHideTarget.classList.toggle("hidden", !this.visibleValue)
+  }
+
+  newText(): string {
+    return this.visibleValue ? "Hide" : "Show"
+  }
+
+  updateText(): void {
+    this.elementWithTextTarget.innerText = this.newText()
   }
 }
